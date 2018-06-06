ad709a4c3a7eafb68c491badfef9d5087a2ae65a
Lahy <1245066197@qq.com>
2018-06-06 16:33:07 +0800

    Add files via upload

---
+++ Text Folder/text3_1.py
@@ -0,0 +1,35 @@
+# 面向对象 OOP(Object Oriented Programming)
+
+# 定义对象
+class Bird(object):
+  have_feather = True
+  way_of_reproduction = 'egg'
+# 获取对象属性
+summer = Bird()
+print(summer.way_of_reproduction)
+
+# 动作
+class Bird1(object):
+  have_feather = True
+  way_of_reproduction = 'egg'
+  # 方法的第一个参数必须是 self
+  def move(self, dx, dy):
+    position = [0,0]
+    position[0] = position[0] + dx
+    position[1] = position[1] + dy
+    return position
+summer = Bird1()
+print('after move',summer.move(5,8))
+
+# 子类
+class Chicken(Bird1):
+  way_of_move = 'walk'
+  possible_in_KFC = True
+
+class Oriole(Bird1):
+  way_of_move = 'fly'
+  possible_in_KFC = False
+
+summer = Chicken()
+print(summer.have_feather)
+print(summer.move(5,8))
---
+++ Text Folder/text3_2.py
@@ -0,0 +1,28 @@
+# 面向对象 扩展
+
+# 调用类的信息
+class Jay(object):
+  laugh = '23333'
+  def show_laugh(self):
+    print(self.laugh)
+  def laugh_1st(self):
+    for i in range(3):
+      self.show_laugh()
+LL = Jay()
+LL.laugh_1st()
+
+# __init__()  special method
+# 初始化
+class me(Jay):
+  def __init__(self,show_laugh):
+    print('That is me',show_laugh)
+summer = me('Lahy')
+
+class Person(object):
+  def __init__(self, input_gender):
+    self.gender = input_gender
+  def printGender(self):
+    print(self.gender)
+FirPer = Person('male')
+print(FirPer.gender)
+FirPer.printGender()
