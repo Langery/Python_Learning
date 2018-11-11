86b7ce4d43583be2749ae6f64838f3e2c5218857
Langery <1245066197@qq.com>
2018-11-11 21:41:32 +0800

    add some code

--- Function/04-HigherOrderFunction.py
+++ Function/04-HigherOrderFunction.py
@@ -5,6 +5,14 @@
 a = int(input('输入任意整数'))
 print(abs(a))
 
-x = abs(int(input('请再次输入任意整数：')))
+b = abs(int(input('请再次输入任意整数：')))
 # 只获取最后的输入值 Num
-print(x)
+print(b)
+
+def add(x, y):
+  return abs(x) + abs(y)
+
+c = int(input('请输入第一个数：'))
+d = int(input('请输入第二个数：'))
+# e = input('请输入方法：')
+print(add(c,d))
